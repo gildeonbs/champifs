8817f7998c8b523a13e5cf9f9c486eb9f4f80d3d
Gildeon Santos <gildeon23@gmail.com>
2025-07-12 14:30:53 -0300

    Projeto criado

---
+++ src/main/java/dev/gildeonbs/champifs/ChampIfsApplication.java
@@ -0,0 +1,13 @@
+package dev.gildeonbs.champifs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampIfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampIfsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gildeonbs/champifs/ChampIfsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gildeonbs.champifs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampIfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
