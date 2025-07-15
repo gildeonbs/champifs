8197c6c7f6b3ed33ee460042d8df37e7ffc2ff16
Gildeon Santos <gildeon23@gmail.com>
2025-07-14 22:49:47 -0300

    DTOs added

---
+++ src/main/java/dev/gildeonbs/champifs/dto/ArbitroDTO.java
@@ -0,0 +1,21 @@
+package dev.gildeonbs.champifs.dto;
+
+import dev.gildeonbs.champifs.entities.ArbitroEntity;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+@Getter
+@Setter
+public class ArbitroDTO {
+
+    private long id;
+    private String nome;
+    private String matricula;
+    //private String senha;
+
+    public ArbitroDTO(ArbitroEntity entity) {
+        BeanUtils.copyProperties(entity, this);
+    }
+
+}
---
+++ src/main/java/dev/gildeonbs/champifs/dto/EsporteDTO.java
@@ -0,0 +1,4 @@
+package dev.gildeonbs.champifs.dto;
+
+public class EsporteDTO {
+}
